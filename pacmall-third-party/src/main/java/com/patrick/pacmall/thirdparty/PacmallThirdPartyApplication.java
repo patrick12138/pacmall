4f7e2b69d057c656b77e383826fccf70e529fd4a
Patrick12138 <xuanweihao6@gmail.com>
2022-05-06 22:16:55 +0800

    增加了OSS相关和文件上传

---
+++ pacmall-third-party/src/main/java/com/patrick/pacmall/thirdparty/PacmallThirdPartyApplication.java
@@ -0,0 +1,13 @@
+package com.patrick.pacmall.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacmallThirdPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacmallThirdPartyApplication.class, args);
+	}
+
+}
---
+++ pacmall-third-party/src/test/java/com/patrick/pacmall/thirdparty/PacmallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.pacmall.thirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacmallThirdPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
