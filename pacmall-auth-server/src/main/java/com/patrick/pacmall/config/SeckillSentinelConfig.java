61b2f031a28d40c1e8d396c0f5c407ccbd058966
Patrick12138 <xuanweihao6@gmail.com>
2022-07-21 18:51:39 +0800

    增加sentinel进行服务熔断

--- pacmall-auth-server/src/main/java/com/patrick/pacmall/config/SeckillSentinelConfig.java
+++ pacmall-auth-server/src/main/java/com/patrick/pacmall/config/SeckillSentinelConfig.java
@@ -1,26 +1,29 @@
-//package com.patrick.pacmall.config;
-//
-//import com.alibaba.fastjson.JSON;
-//import com.patrick.common.exception.BizCodeEnume;
-//import com.patrick.common.utils.R;
-//import org.springframework.context.annotation.Configuration;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//
-//public class SeckillSentinelConfig {
-//
-//    public SeckillSentinelConfig(){
-//        WebCallbackManager.setUrlBlockHandler(new UrlBlockHandler() {
-//            @Override
-//            public void blocked(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws IOException {
-//                R error = R.error(BizCodeEnume.TOO_MANY_REQUEST.getCode(), BizCodeEnume.TOO_MANY_REQUEST.getMsg());
-//                httpServletResponse.setCharacterEncoding("UTF-8");
-//                httpServletResponse.setContentType("application/json");
-//                httpServletResponse.getWriter().write(JSON.toJSONString(error));
-//            }
-//        });
-//    }
-//}
+package com.patrick.pacmall.config;
+
+import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
+import com.alibaba.csp.sentinel.adapter.servlet.callback.WebCallbackManager;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.fastjson.JSON;
+import com.patrick.common.exception.BizCodeEnume;
+import com.patrick.common.utils.R;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class SeckillSentinelConfig {
+
+    public SeckillSentinelConfig(){
+        WebCallbackManager.setUrlBlockHandler(new UrlBlockHandler() {
+            @Override
+            public void blocked(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws IOException {
+                R error = R.error(BizCodeEnume.TOO_MANY_REQUEST.getCode(), BizCodeEnume.TOO_MANY_REQUEST.getMsg());
+                httpServletResponse.setCharacterEncoding("UTF-8");
+                httpServletResponse.setContentType("application/json");
+                httpServletResponse.getWriter().write(JSON.toJSONString(error));
+            }
+        });
+    }
+}
--- pacmall-seckill/src/main/java/com/patrick/pacmall/seckill/config/SeckillSentinelConfig.java
+++ pacmall-seckill/src/main/java/com/patrick/pacmall/seckill/config/SeckillSentinelConfig.java
@@ -6,15 +6,15 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import com.patrick.common.exception.BizCodeEnume;
 import com.patrick.common.utils.R;
+import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@Configuration
+@Configuration
 public class SeckillSentinelConfig {
-
-    public SeckillSentinelConfig(){
+    public SeckillSentinelConfig() {
         WebCallbackManager.setUrlBlockHandler(new UrlBlockHandler() {
             @Override
             public void blocked(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws IOException {
